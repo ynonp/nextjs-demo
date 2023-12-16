acb4cd11e553ca9aae943e613902fbe7c2e5bc46
ynonp <ynonperek@gmail.com>
2023-12-16 16:58:24 +0200

    add revalidation

--- src/app/db/posts.ts
+++ src/app/db/posts.ts
@@ -2,12 +2,15 @@
 
 import { PrismaClient } from '@prisma/client'
 import { getSession } from '@auth0/nextjs-auth0';
+import { revalidatePath } from 'next/cache'
+
 const prisma = new PrismaClient()
 
 export async function createPost(text: string) {
   const session = await getSession();
   const author = session?.user?.name || "[Guest User]";
-
+  revalidatePath('/')
+  
   const res = await prisma.post.create({
     data: {
       author,
